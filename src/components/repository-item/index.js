b0acb85921db3234e5580bda5d1a7eb4338abefe
AndrehAguiar <andreaguiar.g@gmail.com>
2021-10-04 22:47:22 -0300

    Publish project

--- src/components/repository-item/index.js
+++ src/components/repository-item/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import * as S from "./styled";
 
-const RepositoryItem = ({ name, linkToRepo, fullName }) => {
+const RepositoryItem = ({ name, linkToRepository, fullName }) => {
   return (
     <S.Wrapper>
       <S.WrapperTitle>{name}</S.WrapperTitle>
       <hr />
       <S.WrapperFullName>full name:</S.WrapperFullName>
-      <S.WrapperLink href={linkToRepo} target="_blank" rel="noreferrer">
+      <S.WrapperLink href={linkToRepository} target="_blank" rel="noreferrer">
         {fullName}
       </S.WrapperLink>
     </S.Wrapper>
